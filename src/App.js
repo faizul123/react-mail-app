8b65378d29b89ae91f01fecba83a46879e501203
Faizul Ahemeed <faizula205@gmail.com>
2019-09-04 22:25:02 +0530

    new controls added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './App.css';
 import { notify } from './components/Notification'
-import {DefaultButton, DefaultButton_SZ2,DefaultButton_SZ3,PrimaryButton,PrimaryButton_SZ2,PrimaryButton_SZ3} from './components/ui-elements/Controls/Controls'
+import {
+  DefaultButton, 
+  DefaultButton_SZ2,
+  DefaultButton_SZ3,
+  PrimaryButton,
+  PrimaryButton_SZ2,
+  PrimaryButton_SZ3, 
+  TextBox, 
+  TextBoxAndLabel, 
+  TextBoxAndIcon, 
+  MaterialTextBox, 
+  CheckBox, 
+  CheckBox2,
+  Select
+} from './components/ui-elements/Controls/Controls'
 
 class App extends React.Component {
   
@@ -21,6 +35,17 @@ class App extends React.Component {
       <PrimaryButton_SZ3 text="I'm Button"/>
       <br/>
       <hr/>
+      <TextBox name="username" size="50" label="Enter your name" />
+      <TextBoxAndLabel name="username" size="30" label="Enter username" />
+      <TextBoxAndIcon name="username" icon="fa fa-spinner fa-spin"/>
+      <MaterialTextBox />
+      <hr/>
+      <span className="warning font-16px">Ex:somedomin@domain.com</span> 
+      <hr/>
+      <CheckBox />
+      <CheckBox2 />
+      <hr/>
+      <Select />
     </div>
     );
   }
--- src/components/ui-elements/Controls/Controls.js
+++ src/components/ui-elements/Controls/Controls.js
@@ -49,4 +49,142 @@ export const PrimaryButton_SZ3 = (props) => {
     );
 };
 
-export default {DefaultButton,DefaultButton_SZ2,DefaultButton_SZ3,PrimaryButton,PrimaryButton_SZ2,PrimaryButton_SZ3};
+export const TextBox = (props) => {
+    return(
+        <div className="form-control">
+            <input
+                type="text" 
+                name={props.name} 
+                size={props.size ? props.size : 30} 
+                className="text-box font-14px"
+                autoComplete="off"
+                {...props}
+                 />
+        </div>
+    );
+}
+
+export const MaterialTextBox = (props) => {
+    return(
+        <div className="form-control">
+            <input
+                type="text" 
+                name={props.name} 
+                size={props.size ? props.size : 30} 
+                className="text-box text-box--material font-14px"
+                autoComplete="off"
+                {...props}
+                 />
+        </div>
+    );
+}
+
+export const TextBoxAndLabel = (props) => {
+    return(
+        <div className="form-control">
+        <span className="form-control--label-holder">
+            <span className="form-control--label">{props.label}</span>
+            <input 
+                type="text" 
+                name={props.name} 
+                size={props.size ? props.size : 30} 
+                className="text-box font-14px"
+                autoComplete="off"
+                 />
+        </span>
+        </div>
+    );
+}
+
+export const TextBoxAndIcon = (props) => {
+    return(
+        <div className="form-control">
+        <span className="form-control--icons-holder">
+            <span className={"form-control--icon " + props.icon }>{(!props.icon) && "#" }</span>
+            <input 
+                type="text" 
+                name={props.name} 
+                size={props.size ? props.size : 30} 
+                className="text-box font-14px"
+                autoComplete="off"
+                 />
+        </span>
+        </div>
+    );
+}
+
+export  class Select extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            list:props.list ? props.list : [{key:"1",value:"Item 1"},{key:"2",value:"Item 2"}]
+        }
+        this.onSelectHandler = this.onSelectHandler.bind(this);
+    }
+
+    onSelectHandler(key){
+        console.log(key);
+        this.setState((state,props) => ({
+            value : "Item 3"
+        })
+    }
+
+    render(){
+        console.log("test")
+        return(
+            <div className="form-control">
+            <span className="form-control--icons-holder">
+                <span className={"form-control--icon " + props.icon }>{(!props.icon) && "#" }</span>
+                    <input 
+                        type="text"
+                        value={this.state.value}
+                        name={props.name} 
+                        size={props.size ? props.size : 30} 
+                        className="selectbox font-14px"
+                        autoComplete="off"
+                        />
+            <ul className="list-container">
+                this.state.list.map(item => {
+                    <li className="list-container--item font-14px" onClick={this.onSelectHandler.bind(null,item.key)} key={item.key}>{item.value}</li>
+                }
+            </ul>
+            </span>
+            </div>
+        );
+    }
+}
+
+export const CheckBox = (props) => {
+    return(
+        <div className="checkbox">
+            <input type="checkbox" value="1" id="checkboxInput" {...props} />
+            <label for="checkboxInput"></label>
+        </div>
+    );
+}
+
+export const CheckBox2 = (props) => {
+    return(
+        <div className="checkbox2">
+            <input type="checkbox" value="1" id="checkboxTwoInput" {...props} />
+            <label for="checkboxTwoInput"></label>
+        </div>
+    );
+}
+
+
+
+export default {
+    DefaultButton,
+    DefaultButton_SZ2,
+    DefaultButton_SZ3,
+    PrimaryButton,
+    PrimaryButton_SZ2,
+    PrimaryButton_SZ3,
+
+    TextBoxAndLabel,
+    TextBoxAndIcon,
+
+    CheckBox,
+    CheckBox2
+};
